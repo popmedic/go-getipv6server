3d8da87e4835bffc0eb45bc8162ca286ab3347ed
Kevin Scardina <kscardina@Kevins-MacBook-Pro.local>
2020-02-01 07:35:32 -0700

    adding server

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
-	
-
-import "fmt"
-
-	
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"strings"
+)
 
 func main() {
-    fmt.Println("hello world")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "<html>", "<body>", "<h1>IP Echo</h1>")
+		defer fmt.Fprint(w, "</body>", "</html>")
+
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if nil != err {
+			fmt.Fprintf(w, "error (%q): %q is not in ip:port format", err, r.RemoteAddr)
+		}
+
+		uIP := net.ParseIP(ip)
+		if nil == uIP {
+			fmt.Fprintf(w, "error: %q is not in ip:port format", ip)
+			return
+		}
+
+		f := r.Header.Get("X-Forwarded-For")
+
+		fmt.Fprint(w, "<p>", uIP, "</p>")
+		fmt.Fprint(w, "<p>Forwarded:<br/>", strings.Join(strings.Split(f, ", "), "<br/>\n"))
+	})
+
+	log.Print("server starting")
+	log.Fatal(http.ListenAndServe(":8099", nil))
 }
